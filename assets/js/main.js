c559d1241c48a5d06cc1aea717c78d713e454d78
Soroush Karimi <sorooshkarimi93@gmail.com>
2022-09-22 18:57:26 +0330

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,45 @@ window.addEventListener('scroll', function () {
   }
 });
 
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@        COUNTDOWN TIMER       @@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// Set the date we're counting down to
+let countDownDate = new Date('Jan 1, 2023 00:00:00').getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function () {
+  // Get today's date and time
+  let now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById('timer').innerHTML =
+    '<h3>' +
+    days +
+    '</h3><h3>' +
+    hours +
+    '</h3><h3>' +
+    minutes +
+    '</h3><h3>' +
+    seconds +
+    '</h3>';
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById('demo').innerHTML = 'EXPIRED';
+  }
+}, 1000);
+
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // @@@@@@@@   scrollrevealjs.org   @@@@@@@
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
